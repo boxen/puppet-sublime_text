95d5ac4faebd89c15256a22843378a0ec5453cac
Mark Tareshawty <tarebyte@gmail.com>
2014-05-26 20:30:38 -0400

    fix system path for ST3

--- spec/classes/sublime_text__config_spec.rb
+++ spec/classes/sublime_text__config_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe 'sublime_text::config' do
   let(:facts) { {:luser => 'testuser'} }
 
-  let(:sublimedir) { "/Users/#{facts[:luser]}/Library/Application Support/Sublime Text" }
+  let(:sublimedir) { "/Users/#{facts[:luser]}/Library/Application Support/Sublime Text 3" }
   let(:packagedir) { "#{sublimedir}/Packages" }
 
   it { should contain_file(sublimedir).with_ensure('directory') }
