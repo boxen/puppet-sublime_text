dc167911dc8aacaf1f19422a92f1546678ccbd2d
Salimane Adjao Moustapha <me@salimane.com>
2016-02-06 19:55:16 +0100

    Support setting package control version and repo

--- manifests/v2.pp
+++ manifests/v2.pp
@@ -4,16 +4,18 @@
 #
 #     include sublime_text::v2
 class sublime_text::v2 ($version = '2.0.2') {
-  package { 'Sublime Text 2':
+  package { "Sublime Text 2-${version}":
+    ensure   => installed,
+    name     => 'Sublime Text 2',
     provider => 'appdmg',
-    source   => "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20${version}.dmg";
+    source   => "https://download.sublimetext.com/Sublime%20Text%20${version}.dmg";
   }
 
   file { "${boxen::config::bindir}/subl2":
     ensure  => link,
     target  => '/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl',
     mode    => '0755',
-    require => Package['Sublime Text 2'],
+    require => Package["Sublime Text 2-${version}"],
   }
 }
 
