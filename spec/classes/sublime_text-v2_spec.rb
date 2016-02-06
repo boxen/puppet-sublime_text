cd6ce3e42539c3d9f07d04c3bc0707ffa7667549
Salimane Adjao Moustapha <me@salimane.com>
2016-02-06 19:56:00 +0100

    Add missing rspec tests

--- spec/classes/sublime_text-v2_spec.rb
+++ spec/classes/sublime_text-v2_spec.rb
@@ -1,10 +1,39 @@
 require 'spec_helper'
 
 describe 'sublime_text::v2' do
-  it do
-    should contain_package('Sublime Text 2').with({
-      :provider => 'appdmg',
-      :source   => 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg'
-    })
+  let(:facts) { 
+    {
+      :boxen_home => "/test/boxen",
+      :boxen_user => "testuser",
+    }
+  }
+
+  context "default params" do
+    it do
+      should contain_package('Sublime Text 2-2.0.2').with({
+        :ensure   => 'installed',
+        :name     => 'Sublime Text 2',
+        :provider => 'appdmg',
+        :source   => 'https://download.sublimetext.com/' +
+                        'Sublime%20Text%202.0.2.dmg'
+      })
+    end
+  end
+
+  context "custom params" do
+    let(:params) {
+      { 
+        :version => '2.1.2'
+      }
+    }
+    it do
+      should contain_package('Sublime Text 2-2.1.2').with({
+        :ensure   => 'installed',
+        :name     => 'Sublime Text 2',
+        :provider => 'appdmg',
+        :source   => 'https://download.sublimetext.com/' +
+                        'Sublime%20Text%202.1.2.dmg'
+      })
+    end
   end
 end
--- spec/classes/sublime_text_spec.rb
+++ spec/classes/sublime_text_spec.rb
@@ -1,12 +1,51 @@
 require 'spec_helper'
 
 describe 'sublime_text' do
-  let(:build_version) { '3083' }
-  it do
-    should contain_package('Sublime Text').with({
-      :provider => 'appdmg',
-      :source   => 'http://c758482.r82.cf2.rackcdn.com/' +
-                   "Sublime%20Text%20Build%20#{build_version}.dmg"
-    })
+  let(:facts) { 
+    {
+      :boxen_home => '/test/boxen',
+      :boxen_user => 'testuser',
+      :luser      => 'testuser'
+    }
+  }
+  context "default params" do
+    it do
+      should contain_package('Sublime Text-Build-3083').with({
+        :ensure   => 'installed',
+        :name     => 'Sublime Text',
+        :provider => 'appdmg',
+        :source   => 'https://download.sublimetext.com/' +
+                        'Sublime%20Text%20Build%203083.dmg'
+      })
+
+      should contain_repository('/Users/testuser/Library/Application Support/Sublime Text 3/Packages/Package Control').with({
+        :ensure  => '5643f4fa9a335a4591482a8ecdf6e0d7413fbaa1',
+        :source  => 'wbond/package_control'
+      }).that_requires('Package[Sublime Text-Build-3083]')
+    end
+  end
+
+  context "custom params" do
+    let(:params) {
+      { 
+        :build                  => '8888',
+        :package_control_ensure => 'v1.2.2',
+        :package_control_source => 'custom/package_control'
+      }
+    }
+    it do
+      should contain_package('Sublime Text-Build-8888').with({
+        :ensure   => 'installed',
+        :name     => 'Sublime Text',
+        :provider => 'appdmg',
+        :source   => 'https://download.sublimetext.com/' +
+                        'Sublime%20Text%20Build%208888.dmg'
+      })
+
+      should contain_repository('/Users/testuser/Library/Application Support/Sublime Text 3/Packages/Package Control').with({
+        :ensure => 'v1.2.2',
+        :source => 'custom/package_control'
+      }).that_requires('Package[Sublime Text-Build-8888]')
+    end
   end
 end
